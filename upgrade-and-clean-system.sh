53585bc85717f07ad6ac7cd9c219ce61bc588dae
stylersnico <stylersnico@gmail.com>
2014-06-07 19:27:06 +0200

    Create upgrade-and-clean-system.sh

---
+++ upgrade-and-clean-system.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#By Nicolas Simond -> http://www.nicolas-simond.com/
+#The Abyss Project -> http://www.abyssproject.net/
+#Compatiblity : Debian 6 + | Ubuntu 12.x + | Debian based distro
+#Use at your own risk !
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo ':: Update the system'
+apt-get update
+
+echo ':: Upgrade the system'
+apt-get upgrade
+
+echo ':: Use autoremove'
+apt-get autoremove
+
+echo ':: Clean the packages'
+apt-get clean
+
+echo ':: Done!'

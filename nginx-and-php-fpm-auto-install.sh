1c99f0f6175fea8ea23681eab31d334e6d132257
Nicolas Simond <stylersnico@gmail.com>
2014-06-08 18:27:58 +0200

    Create nginx-and-php-fpm-auto-install.sh

---
+++ nginx-and-php-fpm-auto-install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#By Nicolas Simond -> http://www.nicolas-simond.com/
+#The Abyss Project -> http://www.abyssproject.net/
+#Compatiblity : Debian 6 + | Ubuntu 12.x + | Debian based distro
+#Use at your own risk !
+# in root terminal : wget -O- https://raw.githubusercontent.com/stylersnico/debian-scripts/master/nginx-and-php-fpm-auto-install.sh | sh
+#Tested on debian 7.5 AMD64
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+clear
+sleep 5 ; echo "update sources list"
+sleep 5
+echo "deb http://ftp.de.debian.org/debian/ wheezy main contrib non-free
+deb-src http://ftp.de.debian.org/debian/ wheezy main contrib non-free
+
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb-src http://security.debian.org/ wheezy/updates main contrib non-free
+
+# wheezy-updates, previously known as 'volatile'
+deb http://ftp.de.debian.org/debian/ wheezy-updates main contrib non-free
+deb-src http://ftp.de.debian.org/debian/ wheezy-updates main contrib non-free
+#NGINX
+deb http://nginx.org/packages/debian/ wheezy nginx
+deb-src http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
+sleep 3
+clear
+echo  ":: Install ginx, PHP-FPM, fcgiwrap et php mods"
+sleep 3 ; apt-get update && apt-get upgrade -y && apt-get install -y nginx php5-fpm php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl fcgiwrap memcached
+sleep 3
+clear
+echo  ":: Done, now restart service and clear the terminal"
+sleep 2 ; service nginx restart && service php5-fpm restart
+sleep 3
+clear

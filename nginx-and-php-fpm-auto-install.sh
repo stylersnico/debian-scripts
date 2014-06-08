fae4221ce932b10cdaa2aad474bf8c330d3b7780
Nicolas Simond <stylersnico@gmail.com>
2014-06-08 19:03:51 +0200

    Update nginx-and-php-fpm-auto-install.sh

--- nginx-and-php-fpm-auto-install.sh
+++ nginx-and-php-fpm-auto-install.sh
@@ -28,7 +28,7 @@ deb-src http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
 sleep 3
 clear
 echo  ":: Install ginx, PHP-FPM, fcgiwrap et php mods"
-sleep 3 ; apt-get update && apt-get upgrade -y && apt-get install -y nginx php5-fpm php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl fcgiwrap memcached
+sleep 3 ; apt-get update && apt-get upgrade -y && apt-get install -y --force-yes nginx php5-fpm php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl fcgiwrap memcached
 sleep 3
 clear
 echo  ":: Done, now restart service and clear the terminal"

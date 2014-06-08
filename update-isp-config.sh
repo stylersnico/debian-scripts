f95d93cefbc374152c07c5b2ebfbd8d57c3b01ed
Nicolas Simond <stylersnico@gmail.com>
2014-06-08 14:10:24 +0200

    Update update-isp-config.sh

--- update-isp-config.sh
+++ update-isp-config.sh
@@ -4,7 +4,7 @@
 #Compatiblity : Debian 6 + | Ubuntu 12.x + | Debian based distro
 #Use at your own risk !
 # in root terminal : wget -O- https://raw.githubusercontent.com/stylersnico/debian-scripts/master/update-isp-config.sh | sh
-#Tested on debian 7.5 AMD64
+#Tested on debian 7.5 AMD64 with ISPConfig 3.0.5.4p1
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
